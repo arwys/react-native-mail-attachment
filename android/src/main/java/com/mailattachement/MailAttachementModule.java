9faf7a2137e879be8c76626e2a8715db596f3094
arwys <a.siregar@expansio.pl>
2023-09-18 02:35:12 +0700

    chore: initial commit

---
+++ android/src/main/java/com/mailattachement/MailAttachementModule.java
@@ -0,0 +1,32 @@
+package com.mailattachement;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = MailAttachementModule.NAME)
+public class MailAttachementModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "MailAttachement";
+
+  public MailAttachementModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  MailAttachementExample
+//
+
+import Foundation
